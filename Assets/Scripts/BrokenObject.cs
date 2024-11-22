87f6b407ca95392cdfb4369f140c77848ba23b2f
Jakub <jak.marcinko@gmail.com>
2024-11-22 12:47:34 +0100

    Added shattering script and vase prefab

---
+++ Assets/Scripts/BrokenObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _shatteredObject;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            Instantiate(_shatteredObject, transform.position, transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+}
